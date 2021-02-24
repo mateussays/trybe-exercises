cb1e1f476950cdeebc0a7dcadf3f9bd7e8bc79f6
Edrei <edreimateus@gmail.com>
2021-02-24 18:51:21 -0300

    Exercicio 2 resolvido

--- introduction/bloco_4/dia_1/scripts.js
+++ introduction/bloco_4/dia_1/scripts.js
@@ -9,3 +9,14 @@ console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
+//Resolvendo exercício 2
+
+let numberOne = 50;
+let numberTwo = 80;
+
+if (numberOne > numberTwo) {
+  console.log('É maior');
+}
+else {
+  console.log('É menor');
+}
