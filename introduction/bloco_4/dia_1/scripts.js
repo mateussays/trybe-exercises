095f82796705c57751d32777a3534ed61b66fa29
Edrei <edreimateus@gmail.com>
2021-02-24 20:48:06 -0300

    Exercicio 5 resolvido

--- introduction/bloco_4/dia_1/scripts.js
+++ introduction/bloco_4/dia_1/scripts.js
@@ -1,53 +1,80 @@
 //Resolvendo exercício 1
+operacoes(70 , 70)
 
-let a = 30;
-let b = 50;
+function operacoes (a, b) {
+  let soma = a + b;
+  console.log('Soma = ' + soma);
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+  let subtracao  = a - b;
+  console.log('Subtração = ' + subtracao);
+
+  let multiplicacao = a * b;
+  console.log('Multiplicação = ' + multiplicacao);
+
+  let divisao = a / b;
+  console.log('Divisao = ' + divisao);
+
+  let modulo = a % b;
+  console.log('Modulo = ' + modulo);
+
+  return true;
+}
 
 //Resolvendo exercício 2
 
-let numberOne = 50;
-let numberTwo = 80;
+function maiorQue(number1 , number2) {
+  if (number1 > number2) {
+    return number1;
+  } 
+  else {
+    return number2;
+  }
+};
 
-if (numberOne > numberTwo) {
-  console.log('É maior');
-}
-else {
-  console.log('É menor');
-}
+console.log(maiorQue(30, 70));
 
 //Resolvendo exercicio 3
 
-let numberThree = 100;
-
-if (numberOne > numberTwo && numberOne > numberThree) {
-  console.log('NumberOne é Maior');
+function maiorQueTres(number1 , number2, number3) {
+  if (number1 > number2 && number1 > number3) {
+    return number1;
+  }
+  else if (number2 > number1 && number2 > number3){
+    return number2;
+  }
+  else {
+    return number3;
+  }
 }
-else if (numberTwo > numberOne && numberTwo > numberThree) {
-  console.log('NumberTwo é Maior');
-}
-else {
-  console.log('NumberThree é maior');
-}
 
-//Resolvendo exercicio 4
+console.log(maiorQueTres(50, 60, 80));
 
-let tenhoDinheiro;
+//Resolvendo exercicio 4 
 
-switch (tenhoDinheiro) {
-  case true:
-    console.log('positive');
-    break;
-  case false:
-    console.log('negative');
-    break;
-  default:
-    console.log('zero');
+function isPositive(number) {
+  if (number > 0) {
+    return 'positive';
+  }
+  else if (number < 0) {
+    return 'negative';
+  }
+  else {
+    return 'zero';
+  }
 }
+console.log(isPositive(60));
 
 //Resolvendo exercicio 5
+
+function somaTriangulo(num1, num2, num3) {
+  let soma = num1 + num2 + num3;
+
+  if (soma >= 180 ) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(somaTriangulo(80, 70, 25));
