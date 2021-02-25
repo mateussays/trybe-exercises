a4989dcd48bddf5418a53ac3d30854ad9d3bde4f
Edrei <edreimateus@gmail.com>
2021-02-24 22:46:49 -0300

    Resolve exercicio 8

--- introduction/bloco_4/dia_1/script2.js
+++ introduction/bloco_4/dia_1/script2.js
@@ -26,3 +26,15 @@ function conversaoNota(nota) {
 
 console.log(conversaoNota(110));
 
+//Resolvendo exercicio 8
+
+function isEven(num1, num2, num3) {
+  if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(isEven(30, 50, 23));
