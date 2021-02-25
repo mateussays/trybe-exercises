540ca07b68ca1922fbb8dae9f8d8d3935945c691
Edrei <edreimateus@gmail.com>
2021-02-25 15:50:32 -0300

    Adiciona resolução do exercicio 5

--- introduction/bloco_4/dia_2/exercicio_4.js
+++ introduction/bloco_4/dia_2/exercicio_4.js
@@ -1,13 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media;
 
 for (let i = 0; i < numbers.length; i++){
- media = (soma += numbers[i]) / numbers.length;
- 
-  if (media >= 20) {
-    console.log('Valor maior que 20');
-  } else {
-    console.log ('Valor menor que 20');
-  }
+   soma += numbers[i]
+};
+
+media = soma / numbers.length;
+
+if (media > 20) {
+  console.log('É maior que 20');
+} else {
+  console.log('É menor que 20')
 };
 
