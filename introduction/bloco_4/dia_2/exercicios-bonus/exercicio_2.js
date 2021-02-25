a283d4a757611ffb94bb5b2629b317adedaeb5ee
Edrei <edreimateus@gmail.com>
2021-02-25 19:29:08 -0300

    Adiciona exercicio 2 bonus resolvido

--- introduction/bloco_4/dia_2/exercicios-bonus/exercicio_2.js
+++ introduction/bloco_4/dia_2/exercicios-bonus/exercicio_2.js
@@ -2,7 +2,7 @@ let numbers = [1, 8, 7, 9, 3, 6, 2, 0, 4, 5];
 
 for (let index = 1; index < numbers.length; index += 1) {
   for (let index2 = 0; index2 < index; index2 += 1) {
-   if (numbers[index] < numbers[index2]) {
+   if (numbers[index] > numbers[index2]) {
      let position = numbers[index];
 
      numbers[index] = numbers[index2];
