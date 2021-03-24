900831052cb4bdc4b7c45d6cd5bafd333047067a
Edrei <edreimateus@gmail.com>
2021-03-24 11:07:16 -0300

    commit final desses exercicios parte II

--- introduction/bloco_07/dia_1/script2.js
+++ introduction/bloco_07/dia_1/script2.js
@@ -27,3 +27,13 @@ let count = 0;
 const input = document.querySelector('#input');
 const button = document.querySelector('#button');
 button.addEventListener('click', ()=> input.value = count += 1);
+
+const changeX = string => {
+  let phrase = 'Tryber x aqui!';
+  let result = phrase.replace('x', string);
+}
+
+changeX('Mateus');
+
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"]
+
