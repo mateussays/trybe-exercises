23817b11fc0c45cfd8c810a6492bacca01418e5f
Edrei <edreimateus@gmail.com>
2021-03-25 12:26:20 -0300

    Adiciona exercicio 2 parte II

---
+++ introduction/bloco_07/dia_2/script2.js
@@ -0,0 +1,25 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addTurno = (lesson, objectKey, value) => {
+  lesson[objectKey] = value;
+}
+
+addTurno(lesson2, 'turno', 'manhã');
