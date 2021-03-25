12f3761042898f4c57c3bff7a3bf40cf1e21fe58
Edrei <edreimateus@gmail.com>
2021-03-25 13:33:47 -0300

    Adiciona exercicio 8 parte II

--- introduction/bloco_07/dia_2/script2.js
+++ introduction/bloco_07/dia_2/script2.js
@@ -43,4 +43,13 @@ const numberOfStudentes = (obj) => {
 console.log(numberOfStudentes(allLessons));
 
 const getValueByNumber = (obj,number) => Object.values(obj)[number];
-console.log(getValueByNumber);
+
+const verifyPair = (obj, key, value) => {
+  const arr = Object.entries(obj);
+  let isEqual = false;
+  for (let index in arr) {
+    if (arr[index][0] === key && arr[index][1] === value) isEqual = true;
+  }
+  return isEqual;
+};
+console.log(verifyPair(lesson2,'professor','Carlos'));
