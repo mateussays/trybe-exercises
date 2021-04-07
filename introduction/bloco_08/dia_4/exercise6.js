c9b0962dc01451dd905d1dfe93c77ff2827748b9
Edrei <edreimateus@gmail.com>
2021-04-07 12:31:02 -0300

    Adiciona exercicio 6 corrigido

--- introduction/bloco_08/dia_4/exercise6.js
+++ introduction/bloco_08/dia_4/exercise6.js
@@ -6,7 +6,7 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 function studentAverage() {
   const nameAndAverage = students.map((student, index) => ({
     name: student,
-    media: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+    average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
   }));
   return nameAndAverage;
 }
