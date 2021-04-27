dd6cd2cb6087df94fbabc63393537981fad5be52
Edrei <edreimateus@gmail.com>
2021-04-27 20:07:48 -0300

    add day exercises

---
+++ front-end/bloco_01/dia_2/dia-2-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Pokedex from './Pokedex';
+import pokemons from './data';
+
+function App() {
+  return (
+   <main className='App'>
+     <h1>Pokedex</h1>
+     <Pokedex pokemons={pokemons} />
+   </main>
+  );
+}
+
+export default App;
